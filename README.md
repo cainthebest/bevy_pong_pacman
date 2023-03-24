a37498f8ef30a2fc2911772a466ea5ddcd7e3c5b
Cain <75994858+cainthebest@users.noreply.github.com>
2023-03-24 01:37:50 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+What was once a meme but is now becoming reality, Coming soon near you.
+
+**Note:** This below is the meme not the actual game.
+
+![i](https://cdn.discordapp.com/attachments/1086790970705723504/1088603593533300736/ece.gif)
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use bevy::prelude::*;
+use systems::*;
+
+mod ball;
+mod components;
+mod paddle;
+mod systems;
+pub struct WinSize {
+    w: f32,
+    h: f32,
+}
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::Rgba {
+            red: (0.),
+            green: (0.),
+            blue: (0.),
+            alpha: (1.0),
+        }))
+        .insert_resource(WindowDescriptor {
+            title: "Pong!".to_string(),
+            width: 700.0,
+            height: 700.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(paddle::PaddlePlugin)
+        .add_plugin(ball::BallPlugin)
+        .add_startup_system(setup)
+        .add_system(move_all_velocity_objects)
+        .add_system(move_all_players.after(move_all_velocity_objects))
+        .add_system(collision_ball.after(move_all_velocity_objects))
+        .run();
+}
+
+fn setup(mut commands: Commands, mut windows: ResMut<Windows>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let window = windows.get_primary_mut().unwrap();
+    let (win_w, win_h) = (window.width(), window.height());
+    let win_size: WinSize = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
+}
